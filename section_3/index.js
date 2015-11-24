06b0efb3ae80716bbe422f0eabef0f896bbd9b9a
Andrii Kozin <z4i.andrey@gmail.com>
2015-11-24 14:35:38 +0100

    Add ejs

--- section_3/index.js
+++ section_3/index.js
@@ -3,17 +3,24 @@ var express = require('express');
 var app = express();
 
 var port = process.env.PORT || 3000;
+app.use('/assets', express.static(__dirname + '/public'));
+app.use('/', function(req, res, next){
+    console.log("Request url: " + req.url);
+    next();
+});
 
-app.get('/', function(req, res) {
-    res.send('<html><head></head><body><h1>Hello world!<h1><h3>Who are you?</h3></body></html>')
+app.set('view engine', 'ejs');
+
+app.get('/person/:person', function(req, res) {
+    res.render('person', { ID: req.params.person });
 });
 
 app.get('/api', function(req, res) {
     res.send({ firstname: 'Andrii', lastname: 'Kozin' })
 });
 
-app.get('/person/:id', function(req, res) {
-    res.send('<html><head></head><body><h1>Hello world!<h1><h3>' + req.params.id + '</h3></body></html>')
+app.get('/', function(req, res) {
+    res.render('index');
 });
 
 app.get('/connect/:id', function(req, res) {
