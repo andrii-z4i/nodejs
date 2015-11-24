0139ec0fe082f6dc8e46baacc2ab723868d65887
Andrii Kozin <z4i.andrey@gmail.com>
2015-11-24 18:26:53 +0100

    Add mongoose use in project

--- section_3/index.js
+++ section_3/index.js
@@ -1,16 +1,14 @@
 var express = require('express');
 var app = express();
 
-var mysql = require('mysql');
-
 var apiController = require('./controllers/apiController');
 var htmlController = require('./controllers/htmlController');
-
 
 var port = process.env.PORT || 3000;
 
 // add middleware layers
 app.use('/assets', express.static(__dirname + '/public'));
+/*var mysql = require('mysql');
 app.use('/', function(req, res, next){
     console.log("Request url: " + req.url);
     var connection = mysql.createConnection({
@@ -24,6 +22,51 @@ app.use('/', function(req, res, next){
     connection.query('SELECT People.ID, Firstname, Lastname, Address FROM People INNER JOIN PersonAddress ON People.ID = PersonAddress.PersonID INNER JOIN Address ON PersonAddress.AddressID = Address.ID ORDER BY People.ID', function(err, rows){
         if (err) throw err;
         console.log(rows);
+    });
+    next();
+});*/
+
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://test:test@ds057244.mongolab.com:57244/addressbook');
+
+var Schema = mongoose.Schema;
+
+var personSchema = new Schema({
+    firstname: String,
+    lastname: String,
+    address: String
+});
+
+var Person = mongoose.model('Person', personSchema);
+
+var john = Person({
+    firstname: 'John',
+    lastname: 'Doe',
+    address: '555 Main St.'
+});
+
+john.save(function(err){
+    if (err) throw err;
+    console.log('person saved!');
+});
+
+var jane = Person({
+    firstname: 'Jane',
+    lastname: 'Doe',
+    address: '555 Main St.'
+});
+
+jane.save(function(err){
+    if (err) throw err;
+    console.log('person saved!');
+});
+
+app.use('/', function(req, res, next){
+    console.log("Request url: " + req.url);
+    Person.find({}, function(err, users){
+        if (err) throw err;
+        console.log(users);
     });
     next();
 });
