6a698db3751ab97a3e179911fc651b5a3d8a54a4
Andrii Kozin <z4i.andrey@gmail.com>
2015-11-24 06:34:08 +0100

    Express module

---
+++ section_3/index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+app.get('/', function(req, res) {
+    res.send('<html><head></head><body><h1>Hello world!<h1><h3>Who are you?</h3></body></html>')
+});
+
+app.get('/api', function(req, res) {
+    res.send({ firstname: 'Andrii', lastname: 'Kozin' })
+});
+
+app.get('/person/:id', function(req, res) {
+    res.send('<html><head></head><body><h1>Hello world!<h1><h3>' + req.params.id + '</h3></body></html>')
+});
+
+
+app.listen(port);
